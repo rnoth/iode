8ea5bf7a4217d3b8ea06f79f86ac967fac6c61b7
josuah <mail@josuah.net>
2016-12-04 23:17:10 +0100

    Fixing the multiplier thing.

--- input.c
+++ input.c
@@ -24,6 +24,8 @@ input(FILE *tty_fp, int tty_fd, Buffer *buffer)
 	/* main execution loop: get input char by char from the keyboard */
 	while (exit_code != EXIT_FAILURE && exit_code != EXIT_SUCCESS) {
 
+		k = fgetc(tty_fp);
+
 		if (ioctl(tty_fd, TIOCGWINSZ, &w) > 0)
 			die("ioctl");
 
@@ -31,14 +33,12 @@ input(FILE *tty_fp, int tty_fd, Buffer *buffer)
 		a->r = w.ws_row;
 		a->b = buffer;
 
-		k = fgetc(tty_fp);
-
 		/* multipliers for commands */
 		while (k >= '0' && k <= '9' && a->m < 1000) {
 			a->m = a->m * 10 + (k - '0');
 			sprintf(a->b->operators, "%d", a->m ? a->m : 0);
 			update_status_line(a->b, a->r, a->c);
-			fgetc(tty_fp);
+			k = fgetc(tty_fp);
 		}
 
 		exit_code = k_pager[(int) k] ? k_pager[(int) k](a) : CONTINUE;
